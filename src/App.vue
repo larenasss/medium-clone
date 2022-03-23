c67063d4e8b955de9c37e8d50c53e74a182157d9
Alexey <ab-vis@mail.ru>
2022-03-23 16:36:09 +0700

    add topbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
+  <app-top-bar></app-top-bar>
   <router-view></router-view>
 </template>
 
 <script>
+import AppTopBar from '@/components/TopBar';
+export default {
+  name: 'App',
+  components: { AppTopBar }
+};
 </script>
---
+++ src/components/TopBar.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav class="navbar navbar-light">
+    <div class="container">
+      <router-link class="navbar-brand" :to="{name: 'home'}">
+        MediumClone
+      </router-link>
+      <ul class="nav navbar-nav pull-xs-right">
+        <li class="nav-item">
+          <router-link class="nav-link" :to="{name: 'home'}" active-class="active">
+            Home
+          </router-link>
+        </li>
+        <template v-if="isLoggedIn">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'createArticle'}" active-class="active">
+              <i class="ion-compose"></i>
+              New Article
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'settings'}" active-class="active">
+              <i class="ion-gear-a"></i>
+              Settings
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'userProfile', params: {slug: currentUser.username}}" active-class="active">
+              <img class="user-pic" :src="currentUser.image">
+              {{ currentUser.username }}
+            </router-link>
+          </li>
+        </template>
+        <template v-if="!isLoggedIn">
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'login'}" active-class="active">
+              Sing in
+            </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" :to="{name: 'register'}" active-class="active">
+              Sing up
+            </router-link>
+          </li>
+        </template>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { computed } from '@vue/runtime-core';
+import { useStore } from 'vuex';
+
+export default {
+  name: 'AppTopBar',
+  setup() {
+    const store = useStore();
+    const currentUser = computed(() => store.state.auth.currentUser);
+    const isLoggedIn = computed(() => store.state.auth.isLoggedIn);
+
+    return {
+      currentUser,
+      isLoggedIn
+    };
+  }
+};
+</script>
