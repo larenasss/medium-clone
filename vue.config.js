323b1f56ed4dea1b92231f652e9a6abf119be7d9
Alexey <ab-vis@mail.ru>
2022-04-06 12:17:39 +0700

    change readme

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   productionSourceMap: false,
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/medium-clone/dist'
-    : '/',
   pages: {
     index: {
-      // entry for the page
       entry: 'src/main.js',
       title: 'Medium Clone'
     }
